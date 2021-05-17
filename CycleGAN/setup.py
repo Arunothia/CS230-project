aab4ff9bef68fd4680b6ec03041030c402ace69d
Arunothia Marappan <armarapp@microsoft.com>
2021-05-17 05:13:52 -0700

    debug

--- CycleGAN/setup.py
+++ CycleGAN/setup.py
@@ -1,13 +1,4 @@
 import matplotlib
-i = 0
-while i < 10:
-    i += 1
-    try:
-        matplotlib.use('TkAgg')
-        break
-    except:
-        print(i)
-
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
