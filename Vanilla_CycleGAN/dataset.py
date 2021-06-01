8bd9d8874ccbbf5508eba7dd49731fd7e84ba256
Arunothia Marappan <armarapp@microsoft.com>
2021-05-31 21:32:58 -0700

    Setting back to train values

--- Vanilla_CycleGAN/dataset.py
+++ Vanilla_CycleGAN/dataset.py
@@ -15,9 +15,9 @@ class PianoFluteDataset(Dataset):
         self.transform = transform
 
         if isTrain:
-            self.flute_images = os.listdir(root_flute)[0: 100]
+            self.flute_images = os.listdir(root_flute)[0: 12994]
         else:
-            self.flute_images = os.listdir(root_flute)[101:121]
+            self.flute_images = os.listdir(root_flute)[12995:12998]
 
         self.piano_images = self.flute_images 
         self.length_dataset = max(len(self.piano_images), len(self.flute_images))
