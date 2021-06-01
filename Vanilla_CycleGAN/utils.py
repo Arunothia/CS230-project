e34b75b2fda48e8e471bc09536329c2d04a6d506
Arunothia Marappan <armarapp@microsoft.com>
2021-05-31 21:16:43 -0700

    debug

--- Vanilla_CycleGAN/utils.py
+++ Vanilla_CycleGAN/utils.py
@@ -23,7 +23,7 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
     def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        fmtstr = '{name} {val' + self.fmt + '}' #({avg' + self.fmt + '})
         return fmtstr.format(**self.__dict__)
 
 
