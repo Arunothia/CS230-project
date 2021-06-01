67510a80be4e4a633be8ef362ce01920c63aef07
Arunothia Marappan <armarapp@microsoft.com>
2021-05-31 17:41:57 -0700

    debug

--- Vanilla_CycleGAN/utils.py
+++ Vanilla_CycleGAN/utils.py
@@ -1,7 +1,46 @@
 import random, torch, os, numpy as np
 import config
 from torchvision.utils import save_image
-import torch_utils
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
 
 
 def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
@@ -23,8 +62,11 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
         param_group["lr"] = lr
 
 def val(gen_F, gen_P, disc_F, disc_P, mse, L1, val_loader, epoch, folder):
-    losses = torch_utils.AverageMeter('Loss', length=10)
-    progress = torch_utils.ProgressMeter(len(val_loader),[losses])
+    losses = AverageMeter('Loss', ':.4e')
+    progress = ProgressMeter(
+        len(val_loader),
+        [losses],
+        prefix='Val: ')
     flute, piano = next(iter(val_loader))
     flute, piano = flute.to(config.DEVICE), piano.to(config.DEVICE)
     gen_F.eval(), gen_P.eval(), disc_F.eval(), disc_P.eval()
@@ -64,7 +106,7 @@ def val(gen_F, gen_P, disc_F, disc_P, mse, L1, val_loader, epoch, folder):
             identity_piano_loss * config.LAMBDA_IDENTITY
         )
 
-        losses.update(G_loss)
+        losses.update(G_loss, 1)
         progress.display(epoch)
 
     gen_F.train(), gen_P.train(), disc_F.train(), disc_P.train()
